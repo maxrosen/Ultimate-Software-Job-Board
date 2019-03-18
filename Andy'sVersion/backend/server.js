9e8cd7e7782b4e518c9b747eee2168dc33c42fac
xinyuzhao <1302167169@qq.com>
2019-03-18 12:20:59 -0400

    Fix client issue added docker files

--- Andy'sVersion/backend/server.js
+++ Andy'sVersion/backend/server.js
@@ -14,17 +14,21 @@ app.use(logger("dev"));
 
 
 
-// Connection URL
+// Connection URL(docker)
+//var url = 'mongodb://mongodb:27017/myproject';
+
+// Connection URL(local)
 var url = 'mongodb://localhost:27017/myproject';
 
 // Use connect method to connect to the server
 mongoose.connect(url,function(err){
-    if(err) console.log("connection error");
+    if(err) console.log("MongoDB connection error");
+    else
     console.log("MongoDB Successfully Connected");
 });
 
 router.get('/get',(req,res)=>{res.send({success: "yes"});});
-router.get('/',(req,res)=>{res.send("BackEnd EntryPoint Listening on 3001");});
+router.get('/',(req,res)=>{res.send("BackEnd EntryPoint Listening on 4000");});
 app.use('/',router);
 app.use('/api/users',user)
 app.listen(API_PORT,()=>console.log("Server listening on 4000"));
