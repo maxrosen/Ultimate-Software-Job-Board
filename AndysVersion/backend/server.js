481ca0978fb48f1f6342667c2f28e166cd7d8218
xinyuzhao <1302167169@qq.com>
2019-03-18 22:36:40 -0400

    Basic Frontend to Backend

--- AndysVersion/backend/server.js
+++ AndysVersion/backend/server.js
@@ -11,7 +11,15 @@ const user = require('./router/api/users');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(logger("dev"));
-
+//Cross Domain
+app.all('*', function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
+    res.header("X-Powered-By", ' 3.2.1');
+    // res.header("Content-Type", "application/json;charset=utf-8");
+    next();
+  });
 
 
 // Connection URL(docker)
