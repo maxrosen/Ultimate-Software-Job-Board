a9445c1f608016019f85441b8b91a3f58b01b89e
Josh Vasilevsky <josh.vasilevsky@gmail.com>
2019-05-01 19:04:56 -0400

    account profile updates

--- frontend/src/components/AccountPage.js
+++ frontend/src/components/AccountPage.js
@@ -13,14 +13,13 @@ class AccountPage extends Component {
     constructor() {
         super();
         this.state = {
-            // questions: [""],
             modalIsOpen: false,
             clicked: false
         };
 
         this.openModal = this.openModal.bind(this);
         this.closeModal = this.closeModal.bind(this);
-        this.onChange = this.onChange.bind(this);
+        
     }
 
 
@@ -32,9 +31,7 @@ class AccountPage extends Component {
         this.setState({ modalIsOpen: false });
     }
 
-    onChange(event) {
-        //not in use right now
-    }
+
 
     render() {
         let user = jwt_decode(localStorage.jwttoken)
@@ -43,58 +40,52 @@ class AccountPage extends Component {
                 <div className="AccountPageConfig">
                     <div className="AccountBar">
                         <div className="space">
-                            <Row className="space">
-                                <ApplyModal key='1' buttonLabel='Add Job' children={<FormGen template={template.work} formfunction={formfunction.createPosition} />} />
-
-                            </Row>
-
+                            <ApplyModal key='1' buttonLabel='Add Job' children={<FormGen template={template.work} formfunction={formfunction.createPosition} />} />
                         </div>
-
                         <div className="space">
                             <Link to="/managejobs">
                                 <Button className="greenButton" size='lg'>Edit Postings</Button>
                             </Link>
                         </div>
-
                         <div className="space">
                             <Link to="/viewapps">
                                 <Button className="greenButton" size='lg'>Manage Applications</Button>
                             </Link>
                         </div>
-
                         <div className="space">
-                            <Row className="space">
-                              <Button className="greenButton" size='lg' onClick={() => { alert('import popup') }}>Import Employees</Button>
-                            </Row>
+                            <Button className="greenButton" size='lg' onClick={() => { alert('import popup') }}>Import Employees</Button>
                         </div>
-                        
-                          <div className="space">
-                            <Row className="space">
-                                <Button className="greenButton" size='lg'>Import Jobs</Button>
-                            </Row>
-
+                        <div className="space">
+                            <Button className="greenButton" size='lg'>Import Jobs</Button>
                         </div>
-
-    
-                            <div className="space">
-                                <CustomQuestionModal key='0' buttonLabel='Manage Questions' buttonLabel='ManageQuestions' children={<FormGen template={template.question} formfunction={formfunction.createQuestion} />} />
-                            </div>
+                        <div className="space">
+                            <CustomQuestionModal key='0' buttonLabel='Manage Questions' buttonLabel='ManageQuestions' children={<FormGen template={template.question} formfunction={formfunction.createQuestion} />} />
+                        </div>
                     </div>
-                </div>
                     <div className="UserProfile">
-                           <div className = "profileInfo">
-                                <Media className="profileImg" src={require('./resources/profile.png')} alt="image"/>
-                            <div className = "profileTextdiv" >
-
+                        <div className="profileCenter">
+                            <div className="profileInfo">
+                                <Media className="profileImg" src={require('./resources/profile.png')} alt="image" />
+                                <div className="profileTextdiv" >
                                     <p className="profileName">Joe Smith</p>
                                     <p className="profileText">Software Engineer</p>
                                     <p className="profileText">Joesmith@slackers.com</p>
                                     <p className="profileText">Employee ID: 123456789</p>
-
+                                </div>
                             </div>
+                            <div className="companyBox">
+                                text
+                        </div>
                         </div>
                     </div>
+                    <div className="recentJobs">
+                        <p className="recentJobsText">Recently Posted</p>
+                        <Container>
+                            stuff
+                        </Container>
+                    </div>
 
+                </div>
                     <Modal
                         isOpen={this.state.modalIsOpen}
                         onRequestClose={this.closeModal}
@@ -120,7 +111,9 @@ const customStyles = {
         marginRight: '-50%',
         transform: 'translate(-50%, -50%)',
         width: '60vh'
-    }
+    },
+    overlay: {zIndex:9999}
+   
 };
 
 export default AccountPage;
