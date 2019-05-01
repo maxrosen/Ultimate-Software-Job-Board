c3b5d961988fe822a62a23097f9373121585ab95
ch403 <cwhuang@umass.edu>
2019-05-01 14:30:40 -0400

    change the custom question into a modal.  Still have issue saving the questions.

--- frontend/src/components/api/formFunction.js
+++ frontend/src/components/api/formFunction.js
@@ -8,3 +8,7 @@ export const createPosition =(newPosition) =>{
 export const createApplication =(newApplication) =>{
     return axios.post('http://localhost:4000/api/applications/create',newApplication).then(res=>console.log(res.data));
 }
+
+export const createQuestion = (newQuestion) => {
+	return axios.post('http://localhost:4000/api/customQuestion/create',newQuestion).then(res=>console.log(res.data));
+}
