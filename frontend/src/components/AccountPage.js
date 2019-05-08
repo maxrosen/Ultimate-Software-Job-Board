1683019f0600c29b457ee82b39ef61a1b798ecdf
tonytran114 <tonytran@umass.edu>
2019-05-08 17:54:32 -0400

    changed accountpage

--- frontend/src/components/AccountPage.js
+++ frontend/src/components/AccountPage.js
@@ -15,152 +15,187 @@ import ReactFileReader from 'react-file-reader';
 import axios from 'axios';
 
 class AccountPage extends Component {
-    constructor() {
-        super();
-        this.state = {
-            modalIsOpen: false,
-            clicked: false
-        };
+	constructor() {
+		super();
+		this.state = {
+			modalIsOpen: false,
+			clicked: false
+		};
 
-        this.openModal = this.openModal.bind(this);
-        this.closeModal = this.closeModal.bind(this);
-    }
+		this.openModal = this.openModal.bind(this);
+		this.closeModal = this.closeModal.bind(this);
+	}
 
-    openModal() {
-        this.setState({ modalIsOpen: true });
-    }
+	openModal() {
+		this.setState({ modalIsOpen: true });
+	}
 
-    closeModal() {
-        this.setState({ modalIsOpen: false });
-    }
+	closeModal() {
+		this.setState({ modalIsOpen: false });
+	}
 
-    handlePositions = files => {
-        var reader = new FileReader();
-        reader.onload = function(e) {
-        // Use reader.result
-            console.log(reader.result);
-            var contents = JSON.parse(reader.result);
-            var i = 0;
-            for(i; i< contents.length; i++){
-                axios.post('http://localhost:4000/api/positions/create',contents[i]).then(res=>console.log(res.data));
-            }
-        }
-        alert("Positions have been added!");
-        reader.readAsText(files[0]);
-    }
+	handlePositions = (files) => {
+		var reader = new FileReader();
+		reader.onload = function(e) {
+			// Use reader.result
+			console.log(reader.result);
+			var contents = JSON.parse(reader.result);
+			var i = 0;
+			for (i; i < contents.length; i++) {
+				axios
+					.post('http://localhost:4000/api/positions/create', contents[i])
+					.then((res) => console.log(res.data));
+			}
+		};
+		alert('Positions have been added!');
+		reader.readAsText(files[0]);
+	};
 
-    handleEmployees = files => {
-        var reader = new FileReader();
-        reader.onload = function(e) {
-        // Use reader.result
-            console.log(reader.result);
-            var contents = JSON.parse(reader.result);
-            var i = 0;
-            for(i; i< contents.length; i++){
-                axios.post('http://localhost:4000/api/users/register',contents[i]).then(res=>console.log(res.data));
-            }
-        }
-        alert("Employees have been added!");
-        reader.readAsText(files[0]);
-    }
+	handleEmployees = (files) => {
+		var reader = new FileReader();
+		reader.onload = function(e) {
+			// Use reader.result
+			console.log(reader.result);
+			var contents = JSON.parse(reader.result);
+			var i = 0;
+			for (i; i < contents.length; i++) {
+				axios
+					.post('http://localhost:4000/api/users/register', contents[i])
+					.then((res) => console.log(res.data));
+			}
+		};
+		alert('Employees have been added!');
+		reader.readAsText(files[0]);
+	};
 
-    render() {
-        let user = jwt_decode(localStorage.jwttoken);
-        if(user.employeeId == 1){
-        return (
-            <Container>
-                <div className="AccountPageConfig">
-                    <div className="AccountBar" align="center">
-                        <h2 align="center">Manager Options</h2>
-                        
-                            <div className="space">
-                                <h3 align="left">Applications</h3>
-                            </div>
-                                <Row className="space">
-                                    <Link to="/viewapps">
-                                        <Button className="greenButton" size='lg'>Open Applications</Button>
-                                    </Link>
-                                </Row>
-                                <Row className="space">
-                                    <CustomQuestionModal key='0' buttonLabel='Custom Questions' children={<FormGen template={template.question} formfunction={formfunction.createQuestion} />} />
-                                </Row>
+	render() {
+		let user = jwt_decode(localStorage.jwttoken);
+		if (user.employeeId == 1) {
+			return (
+				<Container>
+					<div className="AccountPageConfig">
+						<div className="AccountBar" align="center">
+							<h2 align="center">Manager Options</h2>
 
-                            <div className="space">
-                                <h3 align="left">Jobs</h3>
-                            </div>
-                                <Row className="space">
-                                    <ApplyModal key='1' buttonLabel='Add Position' children={<FormGen template={template.work} formfunction={formfunction.createPosition} />} />
-                                </Row>
-                                <Row className="space">
-                                    <Link to="/managejobs">
-                                        <Button className="greenButton" size='lg'>Edit Positions</Button>
-                                    </Link>
-                                </Row>
+							<div className="space">
+								<h3 align="left">Applications</h3>
+							</div>
+							<Row className="space">
+								<Link to="/viewapps">
+									<Button className="greenButton" size="lg">
+										Open Applications
+									</Button>
+								</Link>
+							</Row>
+							<Row className="space">
+								<CustomQuestionModal
+									key="0"
+									buttonLabel="Custom Questions"
+									children={
+										<FormGen
+											template={template.question}
+											formfunction={formfunction.createQuestion}
+										/>
+									}
+								/>
+							</Row>
 
-                            <div className="space">
-                                <h3 align="left">Import Data</h3>
-                            </div>
-                                <Row className="space">
-                                    <ImportPosModal key='1' buttonLabel='Import Positions' 
-                                    children={<ReactFileReader handleFiles={this.handlePositions} fileTypes={'.json'}> <button className="greenButton">Upload</button> </ReactFileReader>} />
-                                </Row>
-                                <Row className="space">
-                                    <ImportEmpModal key='1' buttonLabel='Import Employees' 
-                                    children={<ReactFileReader handleFiles={this.handleEmployees} fileTypes={'.json'}> <button className="greenButton">Upload</button> </ReactFileReader>} />
-                                </Row>   
-                    </div>
+							<div className="space">
+								<h3 align="left">Jobs</h3>
+							</div>
+							<Row className="space">
+								<ApplyModal
+									key="1"
+									buttonLabel="Add Position"
+									children={
+										<FormGen template={template.work} formfunction={formfunction.createPosition} />
+									}
+								/>
+							</Row>
+							<Row className="space">
+								<Link to="/managejobs">
+									<Button className="greenButton" size="lg">
+										Edit Positions
+									</Button>
+								</Link>
+							</Row>
 
-                    <div className="UserProfile">
-                        <div className="profileCenter">
-                            <div className="profileInfo">
-                                <Media className="profileImg" src={require('./resources/profile.png')} alt="image" />
-                                <div className="profileTextdiv">
-                                    <p className="profileName">
-                                        {user.first_name} {user.last_name}
-                                    </p>
-                                    <p className="profileText">{user.title}</p>
-                                    <p className="profileText">{user.email}</p>
-                                </div>
-                            </div>
+							<div className="space">
+								<h3 align="left">Import Data</h3>
+							</div>
+							<Row className="space">
+								<ImportPosModal
+									key="1"
+									buttonLabel="Import Positions"
+									children={
+										<ReactFileReader handleFiles={this.handlePositions} fileTypes={'.json'}>
+											{' '}
+											<button className="greenButton">Upload</button>{' '}
+										</ReactFileReader>
+									}
+								/>
+							</Row>
+							<Row className="space">
+								<ImportEmpModal
+									key="1"
+									buttonLabel="Import Employees"
+									children={
+										<ReactFileReader handleFiles={this.handleEmployees} fileTypes={'.json'}>
+											{' '}
+											<button className="greenButton">Upload</button>{' '}
+										</ReactFileReader>
+									}
+								/>
+							</Row>
+						</div>
 
+						<div className="UserProfile">
+							<div className="profileCenter">
+								<div className="profileInfo">
+									<Media
+										className="profileImg"
+										src={require('./resources/profile.png')}
+										alt="image"
+									/>
+									<div className="profileTextdiv">
+										<p className="profileName">
+											{user.first_name} {user.last_name}
+										</p>
+										<p className="profileText">{user.title}</p>
+										<p className="profileText">{user.companyName}</p>
+										<p className="profileText">{user.email}</p>
+									</div>
+								</div>
 
-
-
-                            <UserQuestions></UserQuestions>
-
-
-
-
-
-                        </div>
-                    </div>
-                    <div className="sideBar"></div>
-
-                </div>
-            </Container>
-        );}
-        else{
-            return(
-                <Container>
-                    <div className="UserProfile">
-                        <div className="profileCenter">
-                            <div className="profileInfo">
-                                <Media className="profileImg" src={require('./resources/profile.png')} alt="image" />
-                                <div className="profileTextdiv" >
-                                    <p className="profileName">{user.first_name} {user.last_name}</p>
-                                    <p className="profileText">{user.title}</p>
-                                    <p className="profileText">{user.email}</p>
-                                </div>
-                            </div>
-                            <div className="companyBox">
-                                Company Bio and Name
-                            </div>
-                        </div>
-                    </div>
-                </Container>
-        );}
-    }
-
+								<UserQuestions />
+							</div>
+						</div>
+						<div className="sideBar" />
+					</div>
+				</Container>
+			);
+		} else {
+			return (
+				<Container>
+					<div className="UserProfile">
+						<div className="profileCenter">
+							<div className="profileInfo">
+								<Media className="profileImg" src={require('./resources/profile.png')} alt="image" />
+								<div className="profileTextdiv">
+									<p className="profileName">
+										{user.first_name} {user.last_name}
+									</p>
+									<p className="profileText">{user.title}</p>
+									<p className="profileText">{user.email}</p>
+								</div>
+							</div>
+							<div className="companyBox">Company Bio and Name</div>
+						</div>
+					</div>
+				</Container>
+			);
+		}
+	}
 }
 const customStyles = {
 	content: {
